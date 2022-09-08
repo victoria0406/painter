bbce23fd7c770ba7518bc001b4d843a2d27fa156
victoria0406 <victoria0406@kaist.ac.kr>
2022-09-08 16:03:47 +0900

    add: setting env variable in .env file

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig((command, mode)=>{
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return{
+    plugins: [vue()],
 
-  server:{
-    port:3000
-  },
-
-  resolve:{
-    alias:{
-      '@' : resolve(__dirname, './src')
+    resolve:{
+      alias:{
+        '@' : resolve(__dirname, './src')
+      },
     },
+    server:{
+      port: process.env.VITE_PORT
+    }
   }
 })
