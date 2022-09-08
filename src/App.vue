8d672a6dec4d8bb3c220113c44c61cd686600d6e
victoria0406 <victoria0406@kaist.ac.kr>
2022-09-08 13:37:25 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
+<template> 
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
