84a22e63e48c60a0429ee738362a2fd3f38b1cb1
victoria0406 <victoria0406@kaist.ac.kr>
2022-09-08 17:06:16 +0900

    add: add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from '@/components/HelloWorld.vue'
-</script>
-
-<template> 
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss" scoped>
-.logo{
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  &:hover{
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  &.vue:hover{
-    filter: drop-shadow(0 0 2em #42b883aa);
-  }
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
-</style>
+</script>
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
 
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from 'vite-plugin-vuetify'
+
+
 export default defineConfig((command, mode)=>{
   process.env = {...process.env, ...loadEnv(mode, process.cwd())};
   return{
-    plugins: [vue()],
+    plugins: [
+		vue(),
+		vuetify({ autoImport: true }),
+	],
 
     resolve:{
       alias:{
